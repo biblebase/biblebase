c54ecca2285a9355f10729eeba2d0ce9cf8ee3eb
Forrest Cao <forrest@factual.com>
2020-07-15 17:41:00 -0700

    Updates

---
+++ precache-manifest.7a2c9d23a16d88373e7cb25b1bbe7a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fcc85f7f16fd17a6853300354fd95f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fcf193e7ce4203f9c0a",
+    "url": "/static/css/main.14551d55.chunk.css"
+  },
+  {
+    "revision": "440c8e72c83fe063fec2",
+    "url": "/static/js/2.14cb6adf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.14cb6adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fcf193e7ce4203f9c0a",
+    "url": "/static/js/main.e6455242.chunk.js"
+  },
+  {
+    "revision": "27caee38b8c860418753",
+    "url": "/static/js/runtime-main.96d51060.js"
+  }
+]);
