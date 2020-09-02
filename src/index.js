056896902467415b277e76b31b075332bb8889cb
Forrest Cao <forrest@factual.com>
2020-09-01 17:08:47 -0700

    orgnizing code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BibleApp from './App';
+import BibleApp from './ui/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
