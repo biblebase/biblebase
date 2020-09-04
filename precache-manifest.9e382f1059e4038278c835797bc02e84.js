f90f5dd5e40d9846f81ff407cc0217c6d56ec17c
Forrest Cao <forrest@factual.com>
2020-09-03 23:19:56 -0700

    Updates

---
+++ precache-manifest.9e382f1059e4038278c835797bc02e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b161fca53f4f52d70ee5a5a1bbf281",
+    "url": "/biblebase/index.html"
+  },
+  {
+    "revision": "fa3139d5a998ef70a513",
+    "url": "/biblebase/static/css/main.6f20f5f2.chunk.css"
+  },
+  {
+    "revision": "bf6833a23272f7622ca3",
+    "url": "/biblebase/static/js/2.c50f0de2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/biblebase/static/js/2.c50f0de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3139d5a998ef70a513",
+    "url": "/biblebase/static/js/main.4d5dd432.chunk.js"
+  },
+  {
+    "revision": "15805b85c38fcfc280d4",
+    "url": "/biblebase/static/js/runtime-main.943abde1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/biblebase/precache-manifest.7e59632b648d696552d21025070b699f.js"
+  "/biblebase/precache-manifest.9e382f1059e4038278c835797bc02e84.js"
 );
 
 self.addEventListener('message', (event) => {
