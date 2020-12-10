cac22eab6478ed08c65bbeb8bf0b4434ab6071c4
Jennifer Chen <shinyi.chen@gmail.com>
2020-12-09 17:16:50 -0800

    Sort verses

--- src/ui/App.js
+++ src/ui/App.js
@@ -10,62 +10,84 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import queryString from "query-string";
 
 class BibleApp extends React.Component {
-
   state = {
-    menuOpen: false
-  }
+    menuOpen: false,
+  };
 
   closeMenu = () => {
     this.setState({
-      menuOpen: false
+      menuOpen: false,
     });
-  }
+  };
 
   openMenu = () => {
     this.setState({
-      menuOpen: true
-    })
-  }
+      menuOpen: true,
+    });
+  };
 
   parseVerses = (query) => {
     const { verses: versesStr } = queryString.parse(query);
-    const verses = versesStr.split(",").map( v => parseInt(v));
+    const verses = versesStr.split(",").map((v) => parseInt(v));
+    verses.sort((a, b) => {
+      if (a === b) return 0;
+      return a < b ? -1 : 1;
+    });
     return verses;
-  }
+  };
 
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL} render={() => (<Redirect to={process.env.PUBLIC_URL + '/1/1'} />)} />
-          <Route path={process.env.PUBLIC_URL + '/:book/:chapter'}
+          <Route
+            exact
+            path={process.env.PUBLIC_URL}
+            render={() => <Redirect to={process.env.PUBLIC_URL + "/1/1"} />}
+          />
+          <Route
+            path={process.env.PUBLIC_URL + "/:book/:chapter"}
             render={(props) => (
               <div className="bible-app">
                 <div className="header">
-                  <Menu bibleIndex={bibleIndex} 
-                        menuOpen={this.state.menuOpen} 
-                        openMenu={this.openMenu} 
-                        closeMenu={this.closeMenu}
-                        book={parseInt(props.match.params.book)}
-                        chapter={parseInt(props.match.params.chapter)} />
+                  <Menu
+                    bibleIndex={bibleIndex}
+                    menuOpen={this.state.menuOpen}
+                    openMenu={this.openMenu}
+                    closeMenu={this.closeMenu}
+                    book={parseInt(props.match.params.book)}
+                    chapter={parseInt(props.match.params.chapter)}
+                  />
                 </div>
                 <div className="body">
                   <div className="body-left">
-                    <ReadingPane bibleIndex={bibleIndex} 
-                                 menuOpen={this.state.menuOpen} 
-                                 closeMenu={this.closeMenu}
-                                 book={parseInt(props.match.params.book)}
-                                 chapter={parseInt(props.match.params.chapter)}
-                                 verses={props.location.search? this.parseVerses(props.location.search) : []}
-                                 history={props.history}/>
+                    <ReadingPane
+                      bibleIndex={bibleIndex}
+                      menuOpen={this.state.menuOpen}
+                      closeMenu={this.closeMenu}
+                      book={parseInt(props.match.params.book)}
+                      chapter={parseInt(props.match.params.chapter)}
+                      verses={
+                        props.location.search
+                          ? this.parseVerses(props.location.search)
+                          : []
+                      }
+                      history={props.history}
+                    />
                   </div>
                   <div className="body-right">
-                    <StudyGuide bibleIndex={bibleIndex} 
-                                menuOpen={this.state.menuOpen} 
-                                closeMenu={this.closeMenu} 
-                                book={parseInt(props.match.params.book)}
-                                chapter={parseInt(props.match.params.chapter)}
-                                verses={props.location.search? this.parseVerses(props.location.search)  : []}/>
+                    <StudyGuide
+                      bibleIndex={bibleIndex}
+                      menuOpen={this.state.menuOpen}
+                      closeMenu={this.closeMenu}
+                      book={parseInt(props.match.params.book)}
+                      chapter={parseInt(props.match.params.chapter)}
+                      verses={
+                        props.location.search
+                          ? this.parseVerses(props.location.search)
+                          : []
+                      }
+                    />
                   </div>
                 </div>
                 <div className="footer">
@@ -74,7 +96,10 @@ class BibleApp extends React.Component {
               </div>
             )}
           />
-          <Route path="/" render={() => (<Redirect to={process.env.PUBLIC_URL + '/1/1'} />)} />
+          <Route
+            path="/"
+            render={() => <Redirect to={process.env.PUBLIC_URL + "/1/1"} />}
+          />
         </Switch>
       </div>
     );
